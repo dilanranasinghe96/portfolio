3c21c3cae78709190650a0541eceec1d806d3e1f
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2023-12-26 20:02:04 +0530

    changes

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import './navbar.css';
+
+const Navbar= () => {
+    return(
+        <nav className='navbar'>
+            <div className='desktopMenu'></div>
+            <button className='desktopMenuBtn'>
+                <img src='' alt='' className='desktopMenuImg' />Contact Me
+            </button>
+        </nav>
+        
+)} 
+
+export default Navbar;
